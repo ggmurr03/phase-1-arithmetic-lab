e9564670a9ce00f72ff55759150395ac5c9865c5
Gregory Murray <gr.j.murray@gmail.com>
2024-10-29 08:55:25 -0700

    added variables

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2
+let num2 = 62/num1
+let num3 = 14
+let num4 = 10
+const multiply = num1*num2
+const random = parseInt(Math.random()*+2 *10);
+const mod = num3%num4;
+const max = Math.max(1, 20);
 
-// Write your code here
+//4 = num3%num4
